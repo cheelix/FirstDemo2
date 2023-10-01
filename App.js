f71f9aaa13806251c260aab4148b8ff904beb70b
cheelix <cheelix@gmail.com>
2023-10-01 14:38:52 -0600

    10.1

--- App.js
+++ App.js
@@ -1,10 +1,38 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image , Button } from 'react-native';
+import { useState } from 'react';
+
+
 
 export default function App() {
+  const[firstName] =  useState('cheelix');
+  const[LastName] =  useState('jung');
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+
+      <View style={styles.header}>
+       
+
+      </View>
+
+
+
+          <View style={styles.container}>
+          <text > I am {firstName} {LastName}.age is .....</text>
+          <StatusBar style='auto' />
+
+
+
+            <Image source={require('./assets/ilaclogo.png')} style={styles.image}/>
+             
+            
+          </View>
+      
+
+
+
+      <Text>222222222222</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -16,5 +44,30 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+   
   },
+header:{
+
+  backgroundColor:'skyblue',
+  padding:130,
+  margin:50,
+
+},
+TextFormat:{
+  fontWeight:'bold',
+
+},
+body:{
+backgroundColor:"tomato",
+},
+image:{
+  width:200,
+  height:200,
+}
+
+
+
+
+
+
 });
